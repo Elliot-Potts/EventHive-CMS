03e83d1e93203c2920d29c99c1a5c8c6b0143ac9
Elliot Potts <elliot.potts@northumbria.ac.uk>
2024-03-17 17:48:21 +0000

    Set Strapi tokens
    Configured the Strapi tokens, away from environment. This is temporary for the debug project.

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: "Hhr3CzG4s8SHR8XjQHfb8g==",
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: "Hhr3CzG4s8SHR8XjQHfb8g==",
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: "Hhr3CzG4s8SHR8XjQHfb8g==",
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
